b2c0cd8faa9a4339a3e6dd4f15aa5459654fb900
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-08-21 17:55:25 +0200

    Setup material-ui + glamorous

--- app/components/App.js
+++ app/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import glamorous from 'glamorous';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const WithFont = glamorous.div({
+  fontFamily: 'Roboto'
+});
 
 const App = () => (
-  <h1>Rust login</h1>
+  <WithFont>
+    <MuiThemeProvider>
+      <RaisedButton label="Rust login" primary />
+    </MuiThemeProvider>
+  </WithFont>
 );
 
 export default App;
